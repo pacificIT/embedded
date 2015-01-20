baf3f45aa78f22979cb31c0de6732f635742eb63
Hai Nguyen <haidai@gmail.com>
2015-01-20 15:35:09 -0800

    More packages that has to be installed to compile the kernel (should be in Google Docs already). Eventually all this will be in the kernel compile script.

---
+++ install_scripts/xda_android_tutorial_dev_tools_install.sh
@@ -0,0 +1,3 @@
+#From XDA tutorial: http://forum.xda-developers.com/showthread.php?t=1748297
+sudo apt-get install git-core gnupg flex bison gperf libsdl-dev libesd0-dev libwxgtk2.8-dev build-essential zip curl libncurses5-dev zlib1g-dev lib32z1-dev lib32ncurses5-dev gcc-multilib g++-multilib lib32z1 lib32ncurses5 lib32bz2-1.0
+
