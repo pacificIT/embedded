abed17eca4eaba26be86e4a9787aa71e87f86ff4
Hai Nguyen <haidai@gmail.com>
2015-01-29 15:14:04 -0800

    Update turtlebot_install.sh
    Added exit after failure and a line to remove it as rosdep install is expected to fail

--- ifc6410/install_scripts/turtlebot_install.sh
+++ ifc6410/install_scripts/turtlebot_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+set -e
 
 #Install turtlebot navigation and other ROS software
 sudo apt-get -y install ros-indigo-navigation ros-indigo-amcl ros-indigo-camera-calibration ros-indigo-calibration ros-indigo-clear-costmap-recovery ros-indigo-cv-bridge ros-indigo-dynamic-reconfigure ros-indigo-hokuyo-node ros-indigo-image-geometry ros-indigo-image-proc ros-indigo-image-view ros-indigo-joy ros-indigo-kobuki ros-indigo-kobuki-description ros-indigo-kobuki-auto-docking ros-indigo-kobuki-bumper2pc ros-indigo-kobuki-node ros-indigo-kobuki-rapps ros-indigo-laser-filters ros-indigo-libg2o ros-indigo-pcl-ros ros-indigo-rocon ros-indigo-rosbridge-server ros-indigo-sensor-msgs ros-indigo-turtlebot ros-indigo-usb-cam sudo ros-indigo-turtlebot-msgs ros-indigo-kobuki-dashboard ros-indigo-urg-c chrony
@@ -19,9 +20,9 @@ git clone -b master https://github.com/RobotWebTools/mjpeg_server.git
 git clone https://github.com/turtlebot/turtlebot.git
 git clone https://github.com/WPI-RAIL/robot_pose_publisher.git
 cd ~/robot2020
+set +e
 rosdep install --from-paths src --ignore-src --rosdistro indigo -y
 catkin_make
-
 #install the udev rules
 source ~/robot2020/devel/setup.bash
 sudo cp ~/robot2020/src/robot2020/config/udev_rules/97-iximove-inforce.rules /etc/udev/rules.d/
